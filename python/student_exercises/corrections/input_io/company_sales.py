fefc1ef2e627d93a2f57f123777ca1134f390ce3
Antonio Pisanello <antonio@apisanello.dev>
2023-11-02 17:32:04 +0100

    Add correction of IO exercise

--- python/student_exercises/corrections/input_io/company_sales.py
+++ python/student_exercises/corrections/input_io/company_sales.py
@@ -1,5 +1,6 @@
 import csv
 import matplotlib.pyplot as plt
+import os
 
 
 def read_sales_data():
@@ -11,8 +12,22 @@ def read_sales_data():
     Returns:
     - sales_data (list): List of dictionaries representing sales data.
     """
-    sales_data = []
+    sales_data_tuple:tuple = []
+    sales_data: list = []
+    current_row: tuple = []
+    full_path = os.path.dirname(os.path.realpath(__file__))
+    full_path = f"{full_path}/sales_data.csv"
+    print(full_path)
+
+    with open(full_path, "r") as file_sales:
+        reader = csv.reader(file_sales)
+        next(reader)
+        sales_data_tuple = [tuple(row) for row in reader]
+        for i in range(len(sales_data_tuple)):
+            current_row = sales_data_tuple[i]
+            sales_data.append({current_row[0]: sales_data_tuple[i]})
     return sales_data
+         
 
 
 def read_employee_data():
@@ -24,7 +39,20 @@ def read_employee_data():
     Returns:
     - employee_data (list): List of dictionaries representing employee data.
     """
-    employee_data = []
+    employee_data_tuple:tuple = []
+    employee_data: list = []
+    current_row: tuple = []
+    full_path = os.path.dirname(os.path.realpath(__file__))
+    full_path = f"{full_path}/employee_data.csv"
+    print(full_path)
+
+    with open(full_path, "r") as file_sales:
+        reader = csv.reader(file_sales)
+        next(reader)
+        employee_data_tuple = [tuple(row) for row in reader]
+        for i in range(len(employee_data_tuple)):
+            current_row = employee_data_tuple[i]
+            employee_data.append({current_row[0]: employee_data_tuple[i]})
     return employee_data
 
 
@@ -38,7 +66,12 @@ def calculate_total_sales(sales_data):
     Returns:
     - total_sales (float): Total sales amount.
     """
-    return None
+    s = 0
+    for sale in sales_data:
+        s += float((list(sale.values())[0])[2])
+    return s
+        
+    
 
 
 def calculate_average_sales(sales_data):
@@ -51,7 +84,7 @@ def calculate_average_sales(sales_data):
     Returns:
     - average_sales (float): Average sales amount.
     """
-    return None
+    return calculate_total_sales(sales_data) / len(sales_data)
 
 def calculate_median_sales(sales_data):
     """
@@ -63,7 +96,10 @@ def calculate_median_sales(sales_data):
     Returns:
     - median_sales (float): Median sales amount.
     """
-    return None
+    list_sales = sorted([float((list(sale.values())[0])[2]) for sale in sales_data])
+    l = len(list_sales)
+    mid = l//2
+    return list_sales[mid] if mid %2 == 1 else (list_sales[mid-1] + list_sales[mid])/2
 
 
 def calculate_total_salary_expenses(employee_data):
@@ -76,7 +112,8 @@ def calculate_total_salary_expenses(employee_data):
     Returns:
     - total_salary_expenses (float): Total salary expenses.
     """
-    return None
+    return sum([float((list(employee.values())[0])[3]) for employee in employee_data])
+        
 
 
 def calculate_average_salary(employee_data):
@@ -89,7 +126,7 @@ def calculate_average_salary(employee_data):
     Returns:
     - average_salary (float): Average salary.
     """
-    return None
+    return calculate_total_salary_expenses(employee_data) / len(employee_data)
 
 def calculate_median_salary(employee_data):
     """
@@ -101,7 +138,10 @@ def calculate_median_salary(employee_data):
     Returns:
     - median_salary (float): Median salary.
     """
-    return None
+    list_salary = [float((list(employee.values())[0])[3]) for employee in employee_data]
+    l = len(list_salary)
+    mid = l//2
+    return list_salary[mid] if mid%2 == 1 else (list_salary[mid-1] + list_salary[mid])/2
 
 
 def find_employee_with_highest_sales(sales_data, employee_data):
@@ -116,21 +156,21 @@ def find_employee_with_highest_sales(sales_data, employee_data):
     - employee_name (str): Name of the employee with the highest sales amount.
     - department_name (str): Name of the department of the employee with the highest sales amount.
     """
-    return None, None
+    se = {}
+    for sale in sales_data:
+        sale_tuple = list(sale.values())[0]
+        if sale_tuple[3] in se:
+            se[sale_tuple[3]] += int(sale_tuple[2])
+        else:
+            se[sale_tuple[3]] = int(sale_tuple[2])
 
+    ma = (0,0)
+    for k, v in se.items():
+      if ma[1] < v:
+          ma = (k, v)
 
-def find_department_with_highest_sales(sales_data, employee_data):
-    """
-    Finds the department with the highest sales.
-
-    Arguments:
-    - sales_data (list): List of dictionaries representing sales data.
-    - employee_data (list): List of dictionaries representing employee data.
-
-    Returns:
-    - department_name (str): Name of the department with the highest sales.
-    """
-    return None
+    employee = (list(employee_data[int(ma[0])].values())[0])
+    return employee[1], employee[2]
 
 
 # def plot_sales_by_department(sales_data, employee_data):
@@ -162,9 +202,11 @@ def find_department_with_highest_sales(sales_data, employee_data):
 def main():
     # Read sales data
     sales_data = read_sales_data()
+    # print(sales_data)
 
     # Read employee data
     employee_data = read_employee_data()
+    # print(employee_data)
 
     # Calculate total sales amount
     total_sales = calculate_total_sales(sales_data)
@@ -195,7 +237,7 @@ def main():
     print("Employee with Highest Sales Amount:", highest_sales_employee, highest_sales_employee_dep)
 
     # Find the department with the highest sales
-    highest_sales_department = find_department_with_highest_sales(sales_data, employee_data)
+    highest_sales_department = highest_sales_employee_dep
     print("Department with Highest Sales:", highest_sales_department)
 
     # Plot total sales by department
