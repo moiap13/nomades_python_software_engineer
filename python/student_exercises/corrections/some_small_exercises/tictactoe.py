e739b3530cef8ee6fb0db7b392981b53cfdbb854
Antonio Pisanello <antonio@apisanello.dev>
2023-11-03 21:39:41 +0100

    add snake exercise

--- python/student_exercises/corrections/some_small_exercises/tictactoe.py
+++ python/student_exercises/corrections/some_small_exercises/tictactoe.py
@@ -1,4 +1,6 @@
 import os
+import random
+import time
 
 # Function to draw the tic-tac-toe board
 def draw_board(board):
@@ -33,31 +35,63 @@ def check_win(board: list, player: str) -> bool:
     Returns:
     - win (bool): True if the player has won, False otherwise.
     """
+    wins = [
+        (0, 1, 2), (3, 4, 5), (6, 7, 8),
+        (0, 3, 6), (1, 4, 7), (2, 5, 8),
+        (0, 4, 8), (2, 4, 6)
+    ]
+    for comp in wins:
+        a, b, c = comp
+        if board[a] == board[b] == board[c] == player:
+            return True
     return False
 
 # Function to play the game
 def play_game():
     board = [' '] * 9
-    current_player = 'X'
+    current_player = 'X' if random.random() > 0.5 else 'O'
     game_over = False
 
     while not game_over:
         # TODO: Draw the board
+        draw_board(board)
 
         # TODO: Get player's move
         # Hint: Use input() to get the move from the player
-        move = None
-
+        
+        try:
+            move = int(input(f"Player {current_player} : Enter number form 1-9 "))
+        except ValueError:
+            print("Enter an integer")
+            time.sleep(0.5)
+            continue
+        
         # TODO: Check if move is valid
         # A valid move is an integer between 1 and 9 (both inclusive)
         # if move is not valid, print "Invalid move. Try again!" and ask for a new moove
+        if not ((move >= 1 and move <= 9) and (board[move-1] == ' ')):
+            print("Enter a valid input")
+            time.sleep(0.5)
+            continue
+        # assert that the move is correct
 
         # TODO: Update the board with the move
         # The borad is the list named board that contains state of the game
+        board[move-1] = current_player
         
         # TODO: Check if the current player has won or draw or continue
         # Hint: Use the check_win() function to check if the current player has won
         # You need to think about something for the draw case
         # if no win and no draw the game continues
+        if check_win(board, current_player): # -> true there is a winner
+            draw_board(board)
+            print(f"Player {current_player} won!")
+            game_over = True
+        elif ' ' not in board: # -> true: there is no more space left in the game -> tie
+            draw_board(board) 
+            print("Tie !")
+            game_over = True
+        else:
+            current_player = "X" if current_player == "O" else "O"
 # Start the game
 play_game()
