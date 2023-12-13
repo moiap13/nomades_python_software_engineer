ae53fa913a0a46b2ef615d8a2a3342c205bd7da4
Antonio Pisanello <antonio@apisanello.dev>
2023-12-13 12:13:26 +0100

    add csv file and some comments in app.py

--- flask/student_exercises/implementations/flask_login/app.py
+++ flask/student_exercises/implementations/flask_login/app.py
@@ -12,6 +12,8 @@ def register():
   if request.method == "POST":
     # TODO: get values from the form 
     # hint: user request.form
+    # then open a csv file and add the new user
+    email = request.form["tbxEmail"] # here be sure that the key of the dictionary is the NAME html attribute of the input
     return redirect(url_for("userinfo"))
   
   return render_template("register.html")
@@ -22,6 +24,13 @@ def userinfo():
   password = session["password"]
   uid = session["uid"]
   return render_template("userinfo.html", email=email, passwod=password, uid=uid)
+
+#TODO: create a route for login that can be accessed by GET and POST
+# when acceding by get only return the login.html file
+# when acceding by POST:
+#  1. get the data from form
+#  2. read the csv file and check if email/password match
+#  3. if there is a match redirect to userinfo
 
 if __name__ == "__main__":
   app.run(debug=True)
