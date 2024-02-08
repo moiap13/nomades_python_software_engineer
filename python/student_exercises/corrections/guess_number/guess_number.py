c578162c872fc5f0f252844706d3112d4b3a29a9
Antonio Pisanello <antonio@apisanello.dev>
2024-02-08 17:37:21 +0100

    add guess number PART 2 correction

--- python/student_exercises/corrections/guess_number/guess_number.py
+++ python/student_exercises/corrections/guess_number/guess_number.py
@@ -6,11 +6,15 @@ def guess_number():
     Function that generates a random number between 1 and 100 and asks the user to guess it.
     :return: None
     """
+    MAX_RANGE = 100
 
     # Generate a random number between 1 and 100
-    secret_number: int = random.randint(1, 100)
+    secret_number: int = random.randint(1, MAX_RANGE)
+    optimal_tries = math.ceil(math.log2(MAX_RANGE))
     print(secret_number)
+
     user_number: int = int(input("Enter a number : "))
+    tries = 1
 
     while user_number != secret_number:
       # si je suis dans le while, j'ai pas trouvé le nombre secret
@@ -20,9 +24,10 @@ def guess_number():
         print("Secret number is higher")
 
       user_number = int(input("Enter a number : ")) 
+      tries += 1 # tries = tries + 1
 
     # si je suis APRES le while, j'ai trouvé le nombre secret
-    print("Bravo, you found the secret number")
+    print(f"{'Bravo, y' if tries <= optimal_tries else 'Y'}ou found the secret number in {tries} {'try' if tries == 1 else 'tries'}")
 
 if __name__ == '__main__':
     # Run the game
