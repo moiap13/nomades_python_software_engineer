780d770403456ec7af1f2ef4bfb46088c773115f
Antonio Pisanello <antonio@apisanello.dev>
2024-02-09 14:43:01 +0100

    Add my_array first corrections

---
+++ python/student_exercises/corrections/basics/my_array/my_array.py
@@ -0,0 +1,183 @@
+def sum(tableau: list[int]) -> int:
+    """
+    Function that returns the sum of the elements of the array
+    :param tableau: the array to sum
+    :return: the sum of the elements of the array
+    """
+    total = 0
+    for curr_elem in tableau:
+        total += curr_elem
+    return total
+
+
+def average(tableau: list[int]) -> float:
+    """
+    Function that returns the average of the elements of the array
+    :param tableau: the array to average
+    :return: the average of the elements of the array
+    """
+    total = 0
+    for curr_elem in tableau:
+        total += curr_elem
+    return total / len(tableau)
+    # return sum(tableau) / len(tableau)
+
+
+def min(tableau: list[int]) -> int:
+    """
+    [10,3,5,2]
+    Function that returns the minimum of the elements of the array
+    :param tableau: the array to find the minimum of
+    :return: the minimum of the elements of the array
+    """
+    # curr_min = 10e10 # math.inf
+    # curr_min = tableau[0]
+    # for curr_elem in tableau:
+    #     if curr_elem < curr_min:
+    #         curr_min = curr_elem
+    # return curr_min
+    # curr_min = tableau[0]
+    # for i in range(1, len(tableau)):
+    #     if tableau[i] < curr_min:
+    #         curr_min = tableau[i]
+    # return curr_min
+    curr_min = tableau[0]
+    for curr_elem in tableau[1:]:
+        if curr_elem < curr_min:
+            curr_min = curr_elem
+    return curr_min
+        
+
+
+
+def max(tableau: list[int]) -> int:
+    """
+    Function that returns the maximum of the elements of the array
+    :param tableau: the array to find the maximum of
+    :return: the maximum of the elements of the array
+    """    
+    curr_min = tableau[0]
+    for curr_elem in tableau[1:]:
+        if curr_elem > curr_min:
+            curr_min = curr_elem
+    return curr_min
+        
+
+
+def min_max(tableau: list[int]) -> tuple[int, int]:
+    """
+    Function that returns the minimum and maximum of the elements of the array
+    :param tableau: the array to find the minimum and maximum of
+    :return: the minimum and maximum of the elements of the array
+    """
+    return None
+
+
+def mode(tableau: list[int]) -> int:
+    """
+    Function that returns the mode of the elements of the array
+    The mode is the value that appears most often in a set of data values.
+    If there is a tie, the mode is the smallest value.
+    :param tableau: the array to find the mode of
+    :return: the mode of the elements of the array
+    """
+    return None
+
+def variance(tableau: list[int]) -> float:
+    """
+    Function that returns the variance of the elements of the array
+    :param tableau: the array to find the variance of
+    :return: the variance of the elements of the array
+    """
+    return None
+
+def standard_deviation(tableau: list[int]) -> float:
+    """
+    Function that returns the standard deviation of the elements of the array
+    The standard deviation is the square root of the variance.
+    :param tableau: the array to find the standard deviation of
+    :return: the standard deviation of the elements of the array
+    """
+    return None
+
+
+def exist(tableau: list[int], valeur: int) -> bool:
+    """
+    Function that returns True if the value exists in the array
+    :param tableau: the array to check if the value exists in
+    :param valeur: the value to check if it exists in the array
+    :return: True if the value exists in the array, False otherwise
+    """
+    for i in tableau:
+        if valeur == i:
+            return True
+    return False
+    # return valeur in tableau
+
+
+def position(tableau: list[int], valeur: int) -> int:
+    """
+    Function that returns the position of the first value in the array
+    If the value does not exist in the array, it returns -1
+    :param tableau: the array to find the position of
+    :param valeur: the value to find the position of
+    :return: the position of the value in the array
+    """
+    for i in range(len(tableau)):
+        curr_elem = tableau[i]
+        if curr_elem == valeur:
+            return i
+    return -1
+
+
+def similars(arr1: list[int], arr2: list[int]) -> bool:
+    """
+    Function that returns True if the two arrays are similar
+    :param arr1: the first array
+    :param arr2: the second array
+    :return: True if the two arrays are similar, False otherwise
+    """
+    return None
+
+
+def is_list(tableau) -> bool:
+    """
+    Function that returns True if the array is a table
+    :param tableau: the array to check if it is a table
+    :return: True if the array is a table, False otherwise
+    """
+    return None
+
+
+def is_list_of_numbers(tableau) -> bool:
+    """
+    Function that returns True if the array is a table of numbers
+    :param tableau: the array to check if it is a table of numbers
+    :return: True if the array is a table of numbers, False otherwise
+    """
+    return None
+
+def sort_ascending(arr: list[int]) -> list[int]:
+    """
+    Function that returns the sorted array in ascending order 
+    :param arr: the array to sort
+    :return: the sorted array in ascending order
+    """
+    return None
+
+
+def sort_descending(arr: list[int]) -> list[int]:
+    """
+    Function that returns the sorted array in descending order 
+    :param arr: the array to sort
+    :return: the sorted array in descending order
+    """
+    return None
+
+def median(tableau: list[int]) -> int:
+    """
+    Function that returns the median of the elements of the array
+    :param tableau: the array to find the median of
+    :return: the median of the elements of the array
+    """
+    return None
