9efbba719adf29c29b73accc1fa290dc9dc56960
Antonio Pisanello <antonio@apisanello.dev>
2024-03-04 17:43:08 +0100

    Add last nppd materials

--- numpy_pandas/examples/data_visualization/test_matplotlib.py
+++ numpy_pandas/examples/data_visualization/test_matplotlib.py
@@ -1,22 +1,19 @@
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-x = [1, 2, 3, 4, 5]
-y = [10, 20, 30, 40, 50]
+# x = ["Jan", "FEB", "MAR", "APR"]
+# x1 = ["Jan", "FEB", "MAR", "APR"]
+# y = [10, 20, 30, 40]
+# y1 = [20, 30, 10, 80]
 
-plt.plot(x, y, color='green', linestyle='dashed', linewidth=2, marker='o', markerfacecolor='blue', markersize=12, label="Test")
+# plt.plot(x, y, '')
+# plt.plot(x1, y1, '')
+# plt.legend(["2021", "2022"])
+# plt.show()
 
-plt.xlabel('x - axis')
-plt.ylabel('y - axis')
-
-plt.legend()
-
-plt.title('My first graph!')
-
-plt.show()
-
-print("End program")
+# print("hello after graph")
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 ax = plt.subplot(projection='3d')
@@ -32,6 +29,43 @@ ax.plot(x, y, z, label='parametric curve')
 ax.legend()
 
 plt.show()
+
+
+
+# import matplotlib.pyplot as plt
+
+# x = [1, 2, 3, 4, 5]
+# y = [10, 20, 30, 40, 50]
+
+# plt.plot(x, y, color='green', linestyle='dashed', linewidth=2, marker='o', markerfacecolor='blue', markersize=12, label="Test")
+
+# plt.xlabel('x - axis')
+# plt.ylabel('y - axis')
+
+# plt.legend()
+
+# plt.title('My first graph!')
+
+# plt.show()
+
+# print("End program")
+
+# import numpy as np
+
+
+# ax = plt.subplot(projection='3d')
+
+# # Prepare arrays x, y, z
+# theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
+# z = np.linspace(-2, 2, 100)
+# r = z**2 + 1
+# x = r * np.sin(theta)
+# y = r * np.cos(theta)
+
+# ax.plot(x, y, z, label='parametric curve')
+# ax.legend()
+
+# plt.show()
 
 # x = ["Jan", "FEB", "MAR", "APR"]
 # x1 = ["Jan", "FEB", "MAR", "APR"]
