8e478bd22b0697406c906e018b41d426c0e431c1
Antonio Pisanello <antonio@apisanello.dev>
2024-03-12 15:22:21 +0100

    add api config

--- rest_api/student_exercises/implementations/todos/app.py
+++ rest_api/student_exercises/implementations/todos/app.py
@@ -1,1 +1,13 @@
 print("Hello World")
+
+
+
+class APIConfig:
+  API_TITLE = "Todo API"
+  API_VERSION = "v1"
+  OPENAPI_VERSION = "3.0.2"
+  OPENAPI_URL_PREFIX = "/"
+  OPENAPI_SWAGGER_UI_PATH = "/docs"
+  OPENAPI_SWAGGER_UI_URL = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+  OPENAPI_REDOC_PATH = "/redoc"
+  OPENAPI_REDOC_UI_URL = "https://cdn.jsdelivr.net/npm/redoc@latest/bundles/redoc.standalone.js"
