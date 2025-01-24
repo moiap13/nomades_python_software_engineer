7fc4ee7d577d6452e0862a264cd27b9661633cad
Antonio Pisanello <antonio@apisanello.dev>
2025-01-24 16:41:16 +0100

    Protected guess_number

--- python/student_exercises/corrections/guess_number/guess_number.py
+++ python/student_exercises/corrections/guess_number/guess_number.py
@@ -7,13 +7,24 @@ def guess_number() -> None:
     Function that generates a random number between 1 and 100 and asks the user to guess it.
     :return: None
     """
+    def get_input() -> int:
+        valid_input: bool = False
+        while not valid_input:
+            try:
+                user_input: int = int(input("enter a number: "))
+            except ValueError:
+                pass
+            else:
+                valid_input = True
+        return user_input
     # for dychtomy optimal search, the formula is the following: log2(100)
 
     # Generate a random number between 1 and 100
     secret_number: int = random.randint(1, MAX_RANGE)
     # selected_number: int = -1
     optimal_tries: int = math.floor(math.log2(MAX_RANGE))
-    selected_number: int = int(input("enter a number: "))
+    selected_number: int = get_input()
+    
     tries: int = 1
     print("you chose the number :", selected_number)
 
@@ -23,7 +34,7 @@ def guess_number() -> None:
         elif selected_number > secret_number:
             print("the secret number is lower")
         
-        selected_number = int(input("enter a number: "))
+        selected_number = get_input()
         print("you chose the number :", selected_number)
         tries += 1
         
